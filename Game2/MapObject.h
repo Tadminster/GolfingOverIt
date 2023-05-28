7938f91f030c5167267b21b9babd527819b7869c
이동화 <hdhd9127@gmail.com>
2023-05-28 20:37:58 +0900

    맵 오브젝트 클래스 추가

---
+++ Game2/MapObject.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#define MAPMAX 38
+#define FLBMAX 4
+class MapObject
+{
+private:
+	class Wall* map[MAPMAX];
+	class Obstacle* floatingBall[FLBMAX];
+	ObImage* backGroundImg;
+	ObImage* wallImg[MAPMAX];
+public:
+	MapObject();
+	~MapObject();
+	void Init();
+	void Update();
+	void LateUpdate();
+	void Render();
+
+};
+
